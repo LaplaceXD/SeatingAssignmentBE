5da76de80c402a84df4772829b45d761688caeb2
Jonh Alexis Buot <ja.buotph@gmail.com>
2023-05-20 06:22:26 +0800

    [Feat][Lex] Added role levels for role access in UserType enums

--- app/Enums/UserType.php
+++ app/Enums/UserType.php
@@ -7,4 +7,16 @@ enum UserType: string
     case Student = 'student';
     case Professor = 'professor';
     case Technician = 'technician';
+
+    public static function getLevel(UserType $type): int
+    {
+        switch ($type) {
+            case self::Student:
+                return 1;
+            case self::Professor:
+                return 2;
+            case self::Technician:
+                return 2;
+        }
+    }
 }

6b1e0746de06e040466b865797de9f7039cac7d6
Jonh Alexis Buot <ja.buotph@gmail.com>
2023-05-21 03:27:24 +0800

    [Misc][Lex] Fixed typo on IssueStatus enum

--- app/Enums/IssueStatus.php
+++ app/Enums/IssueStatus.php
@@ -5,7 +5,7 @@ namespace App\Enums;
 enum IssueStatus: string
 {
     case Raised = 'raised';
-    case Validatated = 'validated';
+    case Validated = 'validated';
     case InProgress = 'in progress';
     case Dropped = 'dropped';
     case Fixed = 'fixed';

9e01bf6546ac6ff553f3143d845f547abe2d33ff
Jonh Alexis Buot <ja.buotph@gmail.com>
2023-05-21 02:56:26 +0800

    [Misc][Lex] Updated the policy name of Users to fit context

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -5,7 +5,7 @@ namespace App\Providers;
 // use Illuminate\Support\Facades\Gate;
 
 use App\Models\User;
-use App\Policies\RolePolicy;
+use App\Policies\UserPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -16,7 +16,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array<class-string, class-string>
      */
     protected $policies = [
-        User::class => RolePolicy::class
+        User::class => UserPolicy::class
     ];
 
     /**

4c085a97054683d9b7f51f7d53096df7fc42ed12
Jonh Alexis Buot <ja.buotph@gmail.com>
2023-05-21 16:24:10 +0800

    [Feat][Lex] Added predictable seeded roles in database

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,7 +14,12 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         User::factory(3)->create(['Role' => UserType::Student]);
+        User::factory(1)->create(['Email' => 'student@example.com', 'Role' => UserType::Student]);
+
         User::factory(1)->create(['Role' => UserType::Professor]);
+        User::factory(1)->create(['Email' => 'professor@example.com', 'Role' => UserType::Professor]);
+
         User::factory(1)->create(['Role' => UserType::Technician]);
+        User::factory(1)->create(['Email' => 'technician@example.com', 'Role' => UserType::Technician]);
     }
 }

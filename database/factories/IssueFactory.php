1629a652c1f7d159686bc78b056e514c52ef40b0
Jonh Alexis Buot <ja.buotph@gmail.com>
2023-05-23 02:09:55 +0800

    [Fix][Lex] Restricted possible values of seeded issues to reasonable dates, and seat numbers

--- database/factories/IssueFactory.php
+++ database/factories/IssueFactory.php
@@ -22,7 +22,7 @@ class IssueFactory extends Factory
     public function definition(): array
     {
         return [
-            'SeatNo' => strtoupper(fake()->randomLetter()) . fake()->randomDigit(),
+            'SeatNo' => strtoupper(chr(rand(0, 5) + ord('a'))) . fake()->randomDigit(),
             'Description' => fake('en')->paragraph(2),
             'ReplicationSteps' => function (array $attributes) {
                 $steps = rand(1, 5);
@@ -35,18 +35,19 @@ class IssueFactory extends Factory
             'CompletedAt' => function (array $attributes) {
                 $isCompleted = in_array($attributes['Status'], IssueStatus::completedCases());
 
-                return $isCompleted ? fake()->datetime() : null;
+                return $isCompleted ? fake()->dateTimeBetween('-30 days') : null;
             },
             'ValidatedAt' => function (array $attributes) {
                 $max = $attributes['CompletedAt']?->format('Y-m-d H:i:s') ?? 'now';
 
-                return array_key_exists('ValidatorID', $attributes) && $attributes['ValidatorID'] ? fake()->dateTime($max) : null;
+                return array_key_exists('ValidatorID', $attributes) && $attributes['ValidatorID']
+                    ? fake()->dateTimeBetween('-45 days', $max) : null;
             },
             'IssuedAt' => function (array $attributes) {
                 $validatedAt = $attributes['ValidatedAt'];
                 $max = $validatedAt?->format('Y-m-d H:i:s') ?? 'now';
 
-                return fake()->dateTime($max);
+                return fake()->dateTimeBetween('-60 days', $max);
             }
         ];
     }
